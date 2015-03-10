8ddcde6df062f8ba5b5afe847e46ab50e9f23891
Jasmine Noack <jasminenoack@gmail.com>
2015-03-10 13:53:56 -0400

    moves files to correct namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, default: {format: :json} do
     resources :users
     resources :sessions, only: [:new, :create, :destroy]
   end
