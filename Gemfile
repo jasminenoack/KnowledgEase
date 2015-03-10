a7c64b62f80dc8a053833519ab944c448f26991b
Jasmine Noack <jasminenoack@gmail.com>
2015-03-10 09:57:19 -0400

    adds backbone skeleton and initializes controllers for users, sessions and static pages

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bcrypt', '~> 3.1.7'
+gem 'backbone-on-rails'
+
 group :development, :test do
   gem 'pry-rails'
   gem 'better_errors'
---
+++ app/assets/javascripts/question_ease.js
@@ -0,0 +1,13 @@
+window.QuestionEase = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  QuestionEase.initialize();
+});
