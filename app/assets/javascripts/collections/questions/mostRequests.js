0f7ec335de01c73524ac67bfdd9e5e1b166fdbbf
jasmine noack <jasminenoack@gmail.com>
2015-03-13 18:05:40 -0400

    adds users stuff

--- app/assets/javascripts/collections/questions/mostRequests.js
+++ app/assets/javascripts/collections/questions/mostRequests.js
@@ -1,4 +1,8 @@
 KnowledgEase.Collections.mostRequested = KnowledgEase.PaginatedCollection.extend({
   url: "api/want_answers/all",
   model: KnowledgEase.Models.Question,
+
+  comparator: function (model) {
+    return model.askers().length * -1
+  }
 })
--- app/assets/javascripts/collections/questions/specificRequests.js
+++ app/assets/javascripts/collections/questions/specificRequests.js
@@ -1,4 +1,8 @@
 KnowledgEase.Collections.specificRequests = KnowledgEase.PaginatedCollection.extend({
   url: "api/want_answers/specific",
   model: KnowledgEase.Models.Question,
+
+  comparator: function (model) {
+    return model.askers().length * -1
+  }
 })
