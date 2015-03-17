6a5d829fcddfa234dd1061a10ec68423a6322f46
Jasmine noack <jasminenoack@gmail.com>
2015-03-17 10:05:36 -0400

    fixes bug that wasn't allowing answering

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -2,7 +2,7 @@ class Api::AnswersController < ApplicationController
   def create
     @answer = current_user.answers.new(answer_params)
     if @answer.save
-      @question = Question.find(:question_id)
+      @question = Question.find(params[:question_id])
       @question.users_following << current_user
       render :show
     else
