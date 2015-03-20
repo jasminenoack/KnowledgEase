44f3e66bb63f82f544012aa7f7aabafb3c27590e
jasminenoack <jasminenoack@gmail.com>
2015-03-20 00:14:58 -0400

    removes phantom end

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
   def create
-    end
     hash = request.env['omniauth.auth']
     user = User.find_by(provider: hash[:provider], uid: hash[:uid])
     unless user
@@ -14,11 +13,10 @@ class SessionsController < ApplicationController
       user.email =  hash[:email]
       user.password = password
       user.password_confirmation = password
-  p "user #{user}"
     end
+    log_in(user)
     user.activated = true
     user.save
-    log_in(user)
     redirect_to root_url
   end
 
