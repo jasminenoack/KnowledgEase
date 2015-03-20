22b2ac4f8722e2771b9f4b1e64c76ca30350102b
jasminenoack <jasminenoack@gmail.com>
2015-03-20 00:32:37 -0400

    changes hash keys

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,18 +7,17 @@ p hash[:provider]
 p hash[:uid]
     user = User.find_by(provider: hash[:provider], uid: hash[:uid])
     unless user
-p "email #{hash[:email]}"
-p hash[:info]
-      user = User.find_by(email: hash[:email])
+p "email #{hash[:info][:email]}"
+      user = User.find_by(email: hash[:info][:email])
     end
     unless user
       password = SecureRandom::urlsafe_base64
       user = User.new
-      user.username = hash[:nickname]
-      user.email =  hash[:email]
+      user.username = hash[:info][:nickname]
+      user.email =  hash[:info][:email]
       user.password = password
       user.password_confirmation = password
-p "email #{hash[:nickname]}"
+p "email #{hash[:info][:nickname]}"
     end
     log_in(user)
     user.activated = true
